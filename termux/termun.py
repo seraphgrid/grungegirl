3267a8c94e5502849e4cfff0140ba501ccab505e
Eristava <90430427+deathslittlegirl@users.noreply.github.com>
2021-10-09 04:29:58 -0400

    Add files via upload

---
+++ termux/termun.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Imports
+import os
+import time
+
+# uninstall
+os.system('clear')
+print("stitching bash shell back together.")
+os.system('cp ~/.bashrc_bak/.bashrc ~')
+time.sleep(2)
+os.system('rm -r ~/.bashrc_bak')
+time.sleep(2)
+print("backup .bashrc file deleted.")
+time.sleep(2)
+os.system('rm -r ~/.grungegirl')
+print("grungegirl removed.")
+time.sleep(1)
+print('version 0.2 - babygirl')
+print('')
+print('i know u love me bitch')
+time.sleep(3)
+os.system('clear')
