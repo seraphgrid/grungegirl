7504ac4d0bd4e2760c8bd1e6b09fd82406863cd5
Eristava <90430427+deathslittlegirl@users.noreply.github.com>
2021-10-08 22:57:52 -0400

    Add files via upload

---
+++ drugs/psychedelics/dmt.py
@@ -0,0 +1,41 @@
+dmtinfo = """N,N-Dimethyltryptamine (also known as DMT, N,N-DMT, Dmitri, and "The Spirit Molecule") is a classical psychedelic substance of the tryptamine class.[1] Despite being one of the simplest psychedelic compounds, it is known for its unique ability to produce short-lived but intense visionary states and complete hallucinations. It is thought to produce its psychedelic effects by binding to serotonin receptors in the brain, although the precise mechanism is not fully understood.
+
+DMT is present in over 65 species of plants and has been identified as being a normal constituent of human metabolism and an endogenous neurotransmitter in certain rodents. Its presence is also known to be widespread throughout the plant kingdom. Although various theories have been postulated, its neurobiological function has yet to be determined.
+
+Depending on the dosage and method of administration, the effects of DMT can range from mild psychedelic states to powerfully immersive life-altering experiences which are often described as the ultimate displacement from ordinary consciousness in which users report experiencing ineffable spiritual realms or alternate dimensions."""
+
+dmtfx = """FX: Spontaeneous bodily sensations
+Tangible euphoria
+Changes in felt gravity
+Spatial disorientation
+Changes felt in bodily form.
+Physical autonomy
+Slight nausea (Tums Recommended)
+Pupil dilation
+Increased heart rate
+Temperature regulation suppression
+Risk of seizure"""
+
+
+dmtrt = "Total RT: 5 - 20min (wtf)"
+dmtae = "AE: 10 - 60min"
+
+dmtlite = "Lite: 10 - 20 mg"
+dmtmed = "Med: 10 - 20 mg"
+dmtmax = "Max: 40 - 60 mg"
+dmtult = "Ult: 60+ mg"
+
+
+def dmt():
+
+    print(dmtinfo)
+    print(dmtfx)
+    print(dmtrt)
+    print(dmtae)
+    print(dmtlite)
+    print(dmtmed)
+    print(dmtmax)
+    print(dmtult)
+
+
+dmt()
---
+++ drugs/weed.py
@@ -0,0 +1,23 @@
+weednfo = "Cannabis (also known as marijuana, weed, pot, grass, herb, and many others) is a preparation of the cannabis plant that produces psychoactive effects when consumed (via smoking, vaporizing, or ingestion). The principal psychoactive constituent of cannabis is tetrahydrocannabinol (THC), which makes up one of 483 known compounds in the plant, including at least 84 other cannabinoids such as cannabidiol (CBD), cannabinol (CBN), tetrahydrocannabivarin (THCV), and cannabigerol (CBG). At least three species are recognized: Cannabis sativa, Cannabis indica, and Cannabis ruderalis."
+
+weedfx = "Subjective effects include sedation, appetite enhancement, immersion enhancement, creativity enhancement, increased sense of humor, increased music appreciation, and euphoria. The effects of cannabis can vary widely depending on dose, strain & form, preexisting tolerance, and set and setting. Notably, it can either strongly suppress or enhance anxiety depending on the individual. Higher doses are reported to produce mild-to-moderate hallucinogenic effects such as visual hallucinations, conceptual thinking and time distortion as well as a greater risk of anxiety, paranoia, and delusions (generally more prevalent with sativa strains)."
+
+weedtime = "Runtime 4 - 10hrs"
+weedae = "After Effects: 6 - 12hrs"
+
+weedlite = "Lite: 2.5 - 5mg"
+weedmed = "Med: 5 - 10mg"
+weedmax = "Max: 10 - 25mg"
+weedult = "Ultimate: 25mg+"
+
+def weed():
+	print("Cannabis")
+	print(weednfo)
+	print(weedfx)
+	print(weedtime)
+	print(weedae)
+	print(weedlite)
+	print(weedmed)
+	print(weedult)
+
+weed()
