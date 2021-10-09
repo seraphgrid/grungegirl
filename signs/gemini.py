73828c550d30a7c3da9ab791da89766ef959c035
Eristava <90430427+deathslittlegirl@users.noreply.github.com>
2021-10-09 11:40:51 -0400

    Add files via upload

---
+++ signs/gemini.py
@@ -0,0 +1,48 @@
+gemnfo = "Gemini is highly thoughtful and built for communication. They hold technology to a higher standard than most people. Emotionally intelligent but very fickle at heart. Symbolized by The Twins. Energetic and quick-witted. Constantly racing mind, prone to anxiety. Highly enjoys dating and flirts a lot. Potential to end up entirely confused in some situations."
+
+gemnfo2 = "Most known for being wholly responsive, even at their worst hour. They can be lively, energetic, honest and adaptable. The intellectual, nerdy twins. Entirely versatile. Lives in their head a lot."
+
+gem_dual = "Duality: Masculine"
+
+gem_trip = "Element: Air"
+
+gem_qual = "Quality: Mutable"
+
+gem_ruler = "Ruler: Mercury"
+
+gem_key = "Keyword: I think"
+
+gem_polar = "Polarity: Sagittarius. Gemini rules thought and communication. Sagittarius thinks on a larger scale, encompassing philosophy and lacking in close involvement."
+
+gem_body = "Part of Body: Hands & Arms"
+
+gem_ld = "Lucky Day: Wednesday"
+
+gem_ln = "Lucky Numbers: 5 & 9"
+
+def gem():
+    print("Gemini ♊")
+    print(" ")
+    print(gemnfo)
+    print(" ")
+    print(gemnfo2)
+    print(" ")
+    print(gem_dual)
+    print(" ")
+    print(gem_trip)
+    print(" ")
+    print(gem_qual)
+    print(" ")
+    print(gem_ruler)
+    print(" ")
+    print(gem_key)
+    print(" ")
+    print(gem_polar)
+    print(" ")
+    print(gem_body)
+    print(" ")
+    print(gem_ld)
+    print(" ")
+    print(gem_ln)
+
+gem()
