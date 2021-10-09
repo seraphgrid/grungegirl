838c35e4fee043994a2a435259b043e7989851eb
Eristava <90430427+deathslittlegirl@users.noreply.github.com>
2021-10-08 22:55:28 -0400

    Add files via upload

---
+++ astrology/planets/moon.py
@@ -0,0 +1,16 @@
+def moon():
+
+    moonInfo = "The moon represents all things related to innate subconscious thought processes, including emotion. It can also deal with collective emotion as well, since it moves so fast that it counts as a social entity. It governs deeply embedded modus operandi, what makes us secure, the inner mother. Strong placements promote receptivity, responsiveness, reflectiveness. It can drive our personalities quite a bit more than the Sun and Mars."
+
+    moonRuler = "Ruler Of: Cancer"
+
+    moonThemes = "Themes: Ectasy, deeply embedded routine, mother, Cancer, breasts, subconscious thought processes including emotion, collective emotion. Security. Our environment."
+
+    print(moonInfo)
+    print("")
+    print(moonRuler)
+    print("")
+    print(moonThemes)
+
+
+moon()
---
+++ astrology/planets/pluto.py
@@ -0,0 +1,16 @@
+def plu():
+
+    pluInfo = "Pluto is the God of the Underworld and governs over humanity's darkest tendencies. Negative placements denote severe trauma or abuse or the tendency to abuse others. Vindication also thrives under Pluto's dominion. Obsession and abrasive sexuality continually appear here. Death is the common subject, used to make metaphorical the process of our transformation. Dig deep and find the truth. It will scare you and you will embrace it. Pluto is the great revealer and will make your head spin. You can discover a consistent and unrelenting struggle for domination."
+
+    pluRuler = "Ruler of: Scorpio"
+
+    pluThemes = "Themes: Change, power struggles, abuse, investigation and research, occultism, destruction, disease, control. Finances. Superconsciousness. Subconscious compartmentalization. Fear. Sex. The dark truth. Dominion."
+
+    print(pluInfo)
+    print("")
+    print(pluRuler)
+    print("")
+    print(pluThemes)
+
+
+plu()
