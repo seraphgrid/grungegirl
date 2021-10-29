4a8dbb86ae077d23cb3e7e84922bc77469c6c5c0
lain <eraseeris@grrlz.net>
2021-10-29 13:25:49 -0400

    0.3 - bladee

---
+++ astrology/points/snode.py
@@ -0,0 +1,22 @@
+def snode():
+    print('')
+    print('S. Node/Ketu')
+    print('')
+    print('''The descending or south node is associated with the past, whether in the karmic sense of things brought 
+    over from past lives into the present one, or in the sense of the influence of the past in the present life, 
+    such as one's inherited traits. The south node may also show an area of life where the person is proficient, 
+    but yet where little further growth will occur: the person's "comfort zone." 
+
+The descending node reflects childhood experiences, particularly those present from an early age. Because they are so 
+taken for granted, the individual may be tempted to fall back into familiar and at times obsessive behaviour patterns 
+when confronted with new challenges. This behaviour may also lead to negative experiences which can leave the child - 
+or later adult - with feelings of inadequacy. When this happens the behaviour pattern can have addictive traits while 
+offering only apparent but not real security. 
+
+An individual remains largely unconscious of the themes associated with the south node while (ideally) striving to 
+achieve the themes associated with the north node. However, this goal can only be reached if a balance is achieved 
+between the two poles.''')
+    print('')
+
+
+snode()
