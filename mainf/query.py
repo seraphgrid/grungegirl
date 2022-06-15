09407e2be2e0de587d2e12a635adeb2f8c317da5
deathslittlegirl <90430427+deathslittlegirl@users.noreply.github.com>
2022-06-15 16:17:28 -0400

    0.5 - telepath

--- mainf/query.py
+++ mainf/query.py
@@ -16,147 +16,65 @@ drug = search
 
 
 if search.lower() == "nothing":
-os.system("browsh http://psychonautwiki.org/")
+    os.system("browsh http://psychonautwiki.org/")
 
 elif search.lower() == "":
-os.system("browsh http://psychonautwiki.org/")
+    os.system("browsh http://psychonautwiki.org/")
 
 elif search.lower() == "search":
-os.system('browsh http://psychonautwiki.org')
-
-# Weed
-if search.lower() == "weed":
-os.system("browsh http://psychonautwiki.org/wiki/W)
-print("Finished searching about weed!")
-
-# Amphet
-
-elif search.lower() == "meth":
-os.system("browsh http://psychonautwiki.org/wiki/Meth")
-print("Finished searching for meth!")
-
-elif search.lower() == "speed":
-os.system("browsh http://psychonautwiki.org/wiki/Speed")
-print("Finished searching for speed!")
-
-elif search.lower() == "mdma":
-os.system("browsh http://psychonautwiki.org/wiki/MDMA")
-print("Finished searching for MDMA!")
-
-
-# Psychedelics
-
-elif search.lower() == "shrooms":
-os.system("browsh http://psychonautwiki.org/wiki/Shrooms")
-print("Finished searching for shrooms!")
-
-elif search.lower() == "lsd":
-os.system("browsh http://psychonautwiki.org/wiki/LSD")
-print("Finished searching for LSD! Hold on to that burrito!")
-
-elif search.lower() == "peyote":
-os.system("browsh http://drugs.tripsit.me/peyote")
-print("Finished searching for peyote (tripsit)!")
-
-elif search.lower() == "salvia":
-os.system("browsh http://psychonautwiki.org/wiki/Salvia")
-print("Finished searching for salvia!")
-
-elif search.lower() == '1p':
-os.system("browsh https://psychonautwiki.org/wiki/1P-LSD")
-print('Finished searching for 1P-LSD!')
-
-# Dissociatives / Downers
-
-elif search.lower() == "dxm":
-os.system("browsh http://psychonautwiki.org/wiki/Dxm")
-print("Finished searching for DXM!")
-
-elif search.lower() == "ambien":
-os.system("browsh http://psychonautwiki.org/wiki/Ambien")
-print("Finished searching for ambien!")
-
-elif search.lower() == "xanax":
-os.system("browsh http://psychonautwiki.org/wiki/Xanax")
-print("Finished searching for xanax!")
-
-elif search.lower() == "alc":
-os.system("browsh http://psychonautwiki.org/wiki/Alcohol")
-print("Finished searching for alcohol! Bleh!")
-
-elif search.lower() == "alcohol":
-os.system("browsh http://psychonautwiki.org/wiki/Alcohol")
-print("Finished searching for alcohol! Bleh!")
-# mdma
-
-# Fuck these drugs
-elif search.lower() == "fentanyl":
-os.system("browsh http://psychonautwiki.org/wiki/Fentanyl")
-print("Finished searching for fentanyl! Slow down, rockstar!")
-
-elif search.lower() == "dph":
-os.system("browsh http://psychonautwiki.org/wiki/Dph")
-print("Finished searching for DPH, you fucking idiot!")
-
-# Coke
-
-elif search.lower() == "coke":
-os.system(
-    "browsh http://psychonautwiki.org/wiki/Cocaine")
-print("Finished searching for cocaine!")
-
-elif search.lower() == 'cocaine':
-os.system('browsh http://psychonautwiki.org/wiki/Cocaine')
-print("Finished searching for cocaine!")
-
+    os.system('browsh http://psychonautwiki.org')
 
 # help
 
 elif search.lower() == "help":
-os.system("browsh https://www.brow.sh/docs/keybindings/")
-print("Finished helping!")
+    os.system("browsh https://www.brow.sh/docs/keybindings/")
+    print("Finished helping!")
 
 # Terraria Wiki (for the lulz also i need it lmao girls gotta game)
 
 elif search.lower() == "terraria":
-os.system(
-    "browsh https://terraria.fandom.com")
-print("Finished searching Terraria!")
+    os.system( "browsh https://terraria.fandom.com")
+    print("Finished searching Terraria!")
 
 # OpenTTD wiki (also need dis)
 
 elif search.lower() == 'ttd':
-os.system("browsh https://wiki.openttd.org/en/")
+    os.system("browsh https://wiki.openttd.org/en/")
 
 # Minecraft wiki (also dis)
 
 elif search.lower() == 'minecraft':
-os.system('browsh https://minecraft.fandom.com/wiki/Minecraft_Wiki')
+    os.system('browsh https://minecraft.fandom.com/wiki/Minecraft_Wiki')
 
 elif search.lower() == 'mc':
-os.system('browsh https://minecraft.fandom.com/wiki/Minecraft_Wiki')
+    os.system('browsh https://minecraft.fandom.com/wiki/Minecraft_Wiki')
 
 # exit software
 
 elif search.lower() == "exit":
-exit("closing debbie.")
+    exit("closing debbie.")
 
 elif search.lower() == "clear":
-os.system('clear')
+    os.system('clear')
 
 elif search.lower() == "close":
-exit("closing debbie.")
+    exit("closing debbie.")
 
 elif search.lower() == "c":
-exit("closing debbie.")
+    exit("closing debbie.")
 
 elif search.lower() == 'a':
-os.system('python ~/.grungegirl/astrology.py')
-exit()
+    os.system('python ~/.grungegirl/astrology.py')
+    exit()
 
 elif search.lower() == 't':
-os.system('python ~/.grungegirl/tarot.py')
-exit()
+    os.system('python ~/.grungegirl/tarot.py')
+    exit()
+
+else:
+    
+    os.system('browsh http://psychonautwiki.org/wiki/' + drug.lower())
 
 
 os.system('python ~/.grungegirl/query.py')
+
