e40ddba0cf7361e2d20376cccd3b8ecb7b208b6a
seraphgrid <90430427+seraphgrid@users.noreply.github.com>
2023-11-13 11:20:04 -0500

    Update mercury.py

--- astrology/planets/mercury.py
+++ astrology/planets/mercury.py
@@ -1,6 +1,6 @@
 def merc():
 
-    mercuryInfo = "Mercury is the planet of analysis and communication. It handles the way that one processes and uses any amount of information given to them. It can also determine the lens through which one handles their feelings or handles negative communication. The strongest placements of Mercury imply the strongest mind. Negative or retrograde placements can denote trouble learning or otherwise communicating."
+    mercuryInfo = "Mercury is the planet of analysis and communication. It handles the way that one processes and uses any amount of information given to them. It can also determine the lens through which one handles their feelings or handles negative communication. The strongest placements of Mercury imply the strongest mind, and where its strongest. Negative or retrograde placements can denote trouble learning or otherwise communicating."
 
     mercuryRuler = "Rules over: Virgo and Gemini."
 
